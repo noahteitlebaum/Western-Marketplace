43e918639d5f2499e46911ade5970948c75a61fa
Johnathon-R <jjr02626@gmail.com>
2025-09-02 17:07:12 -0400

    Signup page create with correct routing and links through navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import NavBar from './components/NavBar'
 import Home from './pages/Home'
 import Browse from './pages/Browse'
 import Sell from './pages/Sell'
+import Signup from './pages/Signup'
+
 import { Routes, Route } from "react-router-dom"
 
 function App() {
@@ -15,6 +17,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/browse" element={<Browse />} />
           <Route path="/sell" element={<Sell />} />
+          <Route path="/signup" element={<Signup/> } />
         </Routes>
       </main> 
     </div>
