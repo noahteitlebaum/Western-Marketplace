5becf449cf5cef777ba6e3b0b969d438d0ff135f
Johnathon-R <jjr02626@gmail.com>
2025-09-06 22:14:34 -0400

    State for form inputs, basic form submission

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,18 +1,35 @@
+import { useState } from "react"
+
 export default function Login() {
-  return(
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    // Check backend for account information.
+  }
+
+  return (
     <div>
       {/** Header */}
       <div className="">
         <h1 className="">Log In</h1>
       </div>
 
-      <form action="">
-        <div>
-          <input type="email" placeholder="Email" />
-        </div>
-        <div>
-          <input type="password" placeholder="Password" />
-        </div>
+      <form action="" onSubmit={handleSubmit}>
+        <input 
+          type="email" 
+          placeholder="Email" 
+          value={email} 
+          onChange={(e)=>setEmail(e.target.value)}
+        />
+        <input 
+          type="password" 
+          placeholder="Password"
+          value={password}
+          onChange={(e)=>setPassword(e.target.value)}
+        />
         <button type="submit">Log In</button>
       </form>
       <a href="/Signup">Sign Up</a>
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,24 +1,59 @@
+import { use, useState } from "react"
+
 export default function Signup() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    console.log({username, email, password})
+
+    // send information to backend to create an account.
+  }
+  
   return (
     <div className="">
+
       {/** Header */}
       <div className="">
         <h1 className="">Sign Up</h1>
       </div>
+
       <div>
-        <form action="">
-          <div>          
-            <input type="text" placeholder="Username" />
-          </div>
-          <div>
-            <input type="email" placeholder="Email"/>
-          </div>
-          <div>
-            <input type="password" placeholder="Password" />
-          </div>
-          <div>
-            <input type="password" placeholder="Confirm Password" />
-          </div>
+        <form action="" onSubmit={handleSubmit}>       
+          <input 
+            type="text" 
+            placeholder="Username"
+            value={username} 
+            onChange={(e)=>setUsername(e.target.value)}
+          />
+          <input 
+            type="email" 
+            placeholder="Email" 
+            value={email} 
+            onChange={(e)=>setEmail(e.target.value)}
+          />
+
+          <input 
+            type="password" 
+            placeholder="Password" 
+            value={password} 
+            onChange={(e)=>setPassword(e.target.value)}
+          />
+          <input 
+            type="password" 
+            placeholder="Confirm Password" 
+            value={confirmPassword} 
+            onChange={(e)=>setConfirmPassword(e.target.value)}
+          />
           <button type="submit">Sign Up</button>
         </form>
       </div>
